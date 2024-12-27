0537cc3fd020f15e69d9bda7e691edf6657454f7
Aitor Donado <aitor.donado@gmail.com>
2024-12-27 12:25:13 +0100

    Añadidos requirements

--- graficos.py
+++ graficos.py
@@ -1,4 +1,27 @@
 import plotly.graph_objs as go
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def grafico_correlacion(data):
+    sns.set_theme(style="white")
+
+    # Compute the correlation matrix
+    corr = data.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, annot=True, cmap=cmap, vmax=.3, center=0,
+                square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
 
 # Función para pintar las reglas en 3D
 def rotable_3d(x, col1, col2, col3, cat_col):
